a3df003f619e9d63206af1a0048bc4aa84e34271
Ygor Sad <ysads2010@gmail.com>
2021-06-24 18:18:04 -0300

    refactor(me): include default_budget_id field

--- app/serializers/api/me_serializer.rb
+++ app/serializers/api/me_serializer.rb
@@ -3,5 +3,9 @@
 module Api
   class MeSerializer < ApplicationSerializer
     attributes :name, :email
+
+    attribute :default_budget_id do |object|
+      object.budgets.first&.id
+    end
   end
 end
--- spec/serializers/api/me_serializer_spec.rb
+++ spec/serializers/api/me_serializer_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 describe Api::MeSerializer do
   it 'serializes using json api' do
     user = create(:user)
+    create(:budget, user: user)
 
     result = described_class.new(user).serializable_hash
 
@@ -15,8 +16,29 @@ describe Api::MeSerializer do
         attributes: {
           email: user.email,
           name: user.name,
+          default_budget_id: user.budgets.first.id,
         },
       },
     )
+  end
+
+  context 'when user does not have a budget yet' do
+    it 'serializes default_budget_id with nil' do
+      user = create(:user)
+
+      result = described_class.new(user).serializable_hash
+
+      expect(result).to eq(
+        data: {
+          id: user.id,
+          type: :me,
+          attributes: {
+            email: user.email,
+            name: user.name,
+            default_budget_id: nil
+          },
+        },
+      )
+    end
   end
 end
