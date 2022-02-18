7895cd06e965dcb986c968d119e01413a39fc66f
Ygor Sad <ysads2010@gmail.com>
2022-02-18 20:55:10 +0100

    refactor: validates if months and monthly budgets totals have correct sign

--- spec/factories/months.rb
+++ spec/factories/months.rb
@@ -5,10 +5,10 @@ FactoryBot.define do
     iso_month do
       IsoMonth.of(Faker::Date.between(from: 2.years.ago, to: Date.today))
     end
-    activity { rand(100_000) }
-    budgeted { rand(100_000) }
+    activity { Faker::Number.within(range: -1_000_00..0) }
+    budgeted { rand(1_000_00) }
     budget
-    income { rand(100_000) }
-    to_be_budgeted { rand(10_000) }
+    income { rand(1_000_00) }
+    to_be_budgeted { rand(100_00) }
   end
 end
