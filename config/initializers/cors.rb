089fcc087ed255925aa17a05a7bce14b4dde27de
Ygor <ysads2010@gmail.com>
2022-06-08 15:40:40 +0200

    chore: prepare to deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:9090'
+    origins *ENV.fetch('CORS_ORIGINS').split(',')
 
     resource '*',
              credentials: true,
