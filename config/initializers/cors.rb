44df46eed58da5e60290ca9e257ef826f112c196
Ygor <ysads2010@gmail.com>
2022-06-09 10:15:12 +0200

    chore: allow regex cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins *ENV.fetch('CORS_ORIGINS').split(',')
+    origins *ENV.fetch('CORS_ORIGINS').split(',').map { |s| Regexp.new(s) }
 
     resource '*',
              credentials: true,
