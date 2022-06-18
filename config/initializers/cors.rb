afac9e822c73dfc00756f5813ec66db8816a684e
Ygor Sad <ysads2010@gmail.com>
2022-06-18 17:59:28 +0200

    chore: add default value to CORS_ORIGINS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins *ENV.fetch('CORS_ORIGINS').split(',').map { |s| Regexp.new(s) }
+    origins *ENV.fetch('CORS_ORIGINS', 'localhost').split(',').map { |s| Regexp.new(s) }
 
     resource '*',
              credentials: true,
